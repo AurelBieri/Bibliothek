bb69dfb455a030d76ec1dfc5e29c289e6a26f535
Lari07 <Larissa_Fahrni@sluz.ch>
2024-01-11 18:05:10 +0100

    added some output data

--- Library/Program.cs
+++ Library/Program.cs
@@ -10,30 +10,42 @@ namespace Library
 	{
 		static void Main(string[] args)
 		{
+			List<Mitarbeiter> mitarbeiters = new List<Mitarbeiter>();
 			Medium b1 = new Buch("TestBuch", 1, 2019, 263, "action", 13);
 			Medium b2 = new Buch("TestBuch Version 2", 2, 2021, 354, "action", 15);
 			Medium D1 = new DVD("TestBuch Version 2", 1, 2021, 120, "HD", "action", 20 );
 			Medium n1 = new NichtAusleihbareMedien("sadfa", 1, 2004);
+			Medium m1 = new Musik("GUTS", 17, 2023, 39, 30, "Olivia Rodrigo", "Pop");
+			Medium h1 = new Hörbuch("Hello", 3, 2000, 13, 34, "Comedy");
+			
 
 
-			Kunde  k1 = new Kunde("Aurel", "Bieri", 16, 1);
+            Kunde  k1 = new Kunde("Aurel", "Bieri", 16, 1);
 
 			 k1.addAusgelhiehen(b1, 6);
 			 k1.addAusgelhiehen(D1, 36);
 			k1.addAusgelhiehen(n1, 223);
 
+			Kunde k2 = new Kunde("Larissa", "Fahrni", 16, 2);
 
-			foreach (IOutput i in k1.getAusgeliehen())
+			k2.addAusgelhiehen(m1, 17);
+            k2.addAusgelhiehen(h1, 3);
+
+            foreach (IOutput i in k1.getAusgeliehen())
 			{
 				Console.WriteLine(i.Output());
 			}
+            foreach (IOutput i in k2.getAusgeliehen())
+            {
+                Console.WriteLine(i.Output());
+            }
 
-			foreach (IAusleihen i in k1.getAusgeliehen())
+            /*foreach (IAusleihen i in k1.getAusgeliehen())
 			{
 				Console.WriteLine(i.ausleihenPreis());
-			}
+			}*/
 
-			Console.ReadKey();
+            Console.ReadKey();
 		}
 	}
 }
