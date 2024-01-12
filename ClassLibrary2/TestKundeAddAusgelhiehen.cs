fbac482d9f175a0766de2280824bc54c46fa84a0
aurel.bieri <bieri.aurel@gmail.com>
2024-01-12 21:07:28 +0100

    Ich habe ein paar neue Tests hinzugefügt.

---
+++ ClassLibrary2/TestKundeAddAusgelhiehen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Library;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    [TestFixture]
+    internal class TestKundeAddAusgelhiehen
+    {
+        [Test]
+        public void TestausleihenPreis1()
+        {
+            Medium b1 = new Buch("TestBuch", 1, 2019, 263, "action", 13);
+            Kunde k1 = new Kunde("Aurel", "Bieri", 16, 1);
+            List<Medium> mediumlist = new List<Medium>();
+
+            k1.addAusgelhiehen(b1, 16);
+            mediumlist = k1.getAusgeliehen();
+
+            string result = mediumlist[0].getName();
+
+            Assert.That(result, Is.EqualTo("TestBuch"));
+        }   
+    }
+}
--- ClassLibrary2/Testoutput.cs
+++ ClassLibrary2/Testoutput.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Library;
 using NUnit.Framework;
+using System.Xml.Linq;
 
 namespace ClassLibrary2
 {
@@ -12,9 +13,28 @@ namespace ClassLibrary2
 	internal class Testoutput
 	{
 		[Test]
-		public void OutputBuchTest()
+		public void OutputBuchTest1()
 		{
+            Medium D1 = new DVD("TestBuch Version 2", 6, 2021, 120, "HD", "action", 54);
+            string result = D1.Output();
+            Assert.That(result, Is.EqualTo("DVD: Name: TestBuch Version 2 with the Id 6 has a duration of 120 Minutes. With a resolution of HD. Genre: action It was released in 2021 Costs: 54"));
+        }
 
-		}
-	}
+        [Test]
+        public void OutputBuchTest2()
+        {
+            Medium b1 = new Buch("TestBuch", 1, 2019, 263, "action", 13);
+            string result = b1.Output();
+            Assert.That(result, Is.EqualTo("Book: Name: TestBuch with the Id 1 has 263 Pages. Genre: action It was released in 2019 Costs: 13"));
+        }
+
+        [Test]
+        public void OutputBuchTest3()
+        {
+            Medium m1 = new Musik("GUTS", 17, 2023, 39, 30, "Olivia Rodrigo", "Pop");
+            string result = m1.Output();
+            Assert.That(result, Is.EqualTo("Musik: Name: GUTS with the Id 17 has a duration of 39 Minutes. It was released in 2023 Costs: 30 Singer: Olivia Rodrigo Style of music: Pop"));
+        }
+
+    }
 }
